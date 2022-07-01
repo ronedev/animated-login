3f27214497732dea91b5e8408a36799642bdcbef
agustin2589 <70908351+rone2589@users.noreply.github.com>
2022-07-01 00:39:51 -0300

    add: form and animation logic

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,14 +1,78 @@
-import React from 'react'
-import { Center, Image, Box } from '@chakra-ui/react'
-import { passwordImages } from '../utils/images'
+import React, { useState } from 'react'
+import { Image, Box, Input, Button, InputGroup, InputRightElement } from '@chakra-ui/react'
+import { passwordImages, writingImages } from '../utils/images'
 
 const Login = () => {
+
+    const [images, setImages] = useState(passwordImages[0])
+    const [show, setShow] = useState(false)
+    const [formValues, setFormValues] = useState({ email: '', password: '' })
+
+    const handleEmail = (e) => {
+        setImages(e.target.value.length >= writingImages.length
+            ? writingImages[writingImages.length - 1]
+            : writingImages[e.target.value.length]
+        )
+        setFormValues(prevValues => ({ ...prevValues, [e.target.name]: e.target.value }))
+    }
+
+    const handlePassword = (e) => {
+        setFormValues(prevValues => ({ ...prevValues, [e.target.name]: e.target.value }))
+    }
+
+    const handlePasswordFocus = ()=>{
+        for(let i = 1; i < passwordImages.length - 1; i++){
+            setTimeout(()=>{
+                setImages(passwordImages[i])
+            }, i * 20)
+        }
+    }
+
+    const handlePasswordBlur = ()=>{
+        for(let i = 1; i < passwordImages.length - 1; i++){
+            setTimeout(()=>{
+                setImages(passwordImages[passwordImages.length - i])
+            }, i * 20)
+        }
+        handleEmail()
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(formValues);
+    }
+
+    const handleClickShow = () => {
+        setShow(!show)
+    }
+
     return (
-        <Center w='35rem' h='50rem' bg='white' borderRadius={15}>
-            <Box bg='#1d9bf0' p={2} borderRadius={100}>
-                <Image src={passwordImages[0]} alt='avatar' w='120px' />
+        <Box w='35rem' h='50rem' bg='white' borderRadius={15} display='grid' alignContent='center' justifyItems='center' gap={4}>
+            <Box bg='#1d9bf0' p={2} borderRadius={100} w='120px'>
+                <Image src={images} alt='avatar' w='120px' />
             </Box>
-        </Center>
+            <Box >
+                <form onSubmit={handleSubmit} style={{ display: 'grid', alignContent: 'center', justifyContent: 'center', gap: '1rem' }}>
+                    <Input type='email' name='email' placeholder='Enter your email' value={formValues.email} onChange={handleEmail} w='300px' />
+                    <InputGroup>
+                        <Input type={show ? 'text' : 'password'} name='password' placeholder='Enter your password' value={formValues.password} onChange={handlePassword} onBlur={handlePasswordBlur} onFocus={handlePasswordFocus} pr={4} />
+                        <InputRightElement width='4.5rem'>
+                            <Button h='1.5rem' size='sm' onClick={()=>{
+                                handleClickShow()
+                                if(show){
+                                    handlePasswordBlur()
+                                }else{
+                                    handlePasswordFocus()
+                                }
+                            }}>
+                                {show ? 'Hide' : 'Show'}
+                            </Button>
+                        </InputRightElement>
+                    </InputGroup>
+                    <Button type='submit' colorScheme='twitter' color='white'>Login</Button>
+                </form>
+            </Box>
+        </Box>
     )
 }
 
--- src/utils/images.js
+++ src/utils/images.js
@@ -16,12 +16,38 @@ import password15 from '../../images/password/15.png'
 import password16 from '../../images/password/16.png'
 import password17 from '../../images/password/17.png'
 
-// import writing1 from '../../images/typing/1.png'
-// import writing1 from '../../images/typing/1.png'
-// import writing1 from '../../images/typing/1.png'
-// import writing1 from '../../images/typing/1.png'
-// import writing1 from '../../images/typing/1.png'
-// import writing1 from '../../images/typing/1.png'
+import writing1 from '../../images/typing/1.png'
+import writing2 from '../../images/typing/2.png'
+import writing3 from '../../images/typing/3.png'
+import writing4 from '../../images/typing/4.png'
+import writing5 from '../../images/typing/5.png'
+import writing6 from '../../images/typing/6.png'
+import writing7 from '../../images/typing/7.png'
+import writing8 from '../../images/typing/8.png'
+import writing9 from '../../images/typing/9.png'
+import writing10 from '../../images/typing/10.png'
+import writing11 from '../../images/typing/11.png'
+import writing12 from '../../images/typing/12.png'
+import writing13 from '../../images/typing/13.png'
+import writing14 from '../../images/typing/14.png'
+import writing15 from '../../images/typing/15.png'
+import writing16 from '../../images/typing/16.png'
+import writing17 from '../../images/typing/17.png'
+import writing18 from '../../images/typing/18.png'
+import writing19 from '../../images/typing/19.png'
+import writing20 from '../../images/typing/20.png'
+import writing21 from '../../images/typing/21.png'
+import writing22 from '../../images/typing/22.png'
+import writing23 from '../../images/typing/23.png'
+import writing24 from '../../images/typing/24.png'
+import writing25 from '../../images/typing/25.png'
+import writing26 from '../../images/typing/26.png'
+import writing27 from '../../images/typing/27.png'
+import writing28 from '../../images/typing/28.png'
+import writing29 from '../../images/typing/29.png'
+import writing30 from '../../images/typing/30.png'
+import writing31 from '../../images/typing/31.png'
+import writing32 from '../../images/typing/32.png'
 
 export const passwordImages = [
     password1,
@@ -43,22 +69,38 @@ export const passwordImages = [
     password17,
 ]
 
-// export const writingImages = [
-//     image1,
-//     image2,
-//     image3,
-//     image4,
-//     image5,
-//     image6,
-//     image7,
-//     image8,
-//     image9,
-//     image10,
-//     image11,
-//     image12,
-//     image13,
-//     image14,
-//     image15,
-//     image16,
-//     image17,
-// ]
+export const writingImages = [
+    password1,
+    writing1,
+    writing2,
+    writing3,
+    writing4,
+    writing5,
+    writing6,
+    writing7,
+    writing8,
+    writing9,
+    writing10,
+    writing11,
+    writing12,
+    writing13,
+    writing14,
+    writing15,
+    writing16,
+    writing17,
+    writing18,
+    writing19,
+    writing20,
+    writing21,
+    writing22,
+    writing23,
+    writing24,
+    writing25,
+    writing26,
+    writing27,
+    writing28,
+    writing29,
+    writing30,
+    writing31,
+    writing32,
+]
